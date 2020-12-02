16f1cd05b25916af765f402e0f6b7ffc551a9c47
aaron markey <markey@hey.com>
2020-12-01 20:38:24 -0500

    Complete 2020.01.2

--- 2020/01.py
+++ 2020/01.py
@@ -21,6 +21,13 @@ For example, suppose your expense report contained the following:
 675
 1456
 In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
 """
 
 inp_raw = """
@@ -237,14 +244,17 @@ def get_left_right(left, right, value):
     return None, None
 
 
-def go(inp, value):
+def get_numbers(inp):
     data = []
     for line in inp.split("\n"):
         num = line.strip()
         if num:
             data.append(int(num))
 
-    data.sort()
+    return data
+
+
+def get_parts(data, value):
     mid = int(value / 2)
 
     mid_idx = -1
@@ -259,9 +269,33 @@ def go(inp, value):
     left_num, right_num = get_left_right(left, right, value)
 
     if left_num and right_num:
-        return left_num * right_num
-    return None
+        return left_num, right_num
+    return None, None
+
+
+def part_one(inp, value):
+    data = get_numbers(inp)
+    data.sort()
+    left, right = get_parts(data, value)
+
+    if left and right:
+        return left, right, left * right
+    return None, None, None
+
+
+def part_two(inp, value):
+    data = get_numbers(inp)
+    data.sort()
+
+    for idx, num in enumerate(data):
+        difference = value - num
+        sub_data = data[: idx + 1] + data[idx + 1 :]
+        left, right = get_parts(sub_data, difference)
+        if left and right:
+            return num, left, right, num * left * right
+    return None, None, None, None
 
 
 if __name__ == "__main__":
-    print(go(inp_raw, 2020))
+    print(part_one(inp_raw, 2020))
+    print(part_two(inp_raw, 2020))
