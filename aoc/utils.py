4ad86e708c89f3705ccfee7030b18c93600a5364
aaron markey <markey@hey.com>
2020-12-03 12:04:28 -0500

    Complete 2020-03

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,12 +1,16 @@
 import pathlib
 
 
-def read_lines(path):
+def read_lines(path, strip=False):
     current_path = pathlib.Path(__file__).parent.absolute()
     with open(f"{current_path}/data/{path}", "r") as file_:
         line = file_.readline()
+
         while line:
-            yield line
+            if strip:
+                yield line.strip()
+            else:
+                yield line
             line = file_.readline()
 
 
