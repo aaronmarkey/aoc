0e2f92f6000c145560e2d7cfb9386d46325c0c5f
aaronmarkey <markey@hey.com>
2021-11-30 19:32:08 -0500

    Prep for AOC 2021

--- aoc/utils.py
+++ aoc/utils.py
@@ -1,9 +1,10 @@
 import pathlib
 
+CURRENT_PATH = pathlib.Path(__file__).parent.absolute().parent.absolute()
+
 
 def read_lines(path, strip=False):
-    current_path = pathlib.Path(__file__).parent.absolute()
-    with open(f"{current_path}/data/{path}", "r") as file_:
+    with open(f"{CURRENT_PATH}/data/{path}", "r") as file_:
         line = file_.readline()
 
         while line:
@@ -15,8 +16,7 @@ def read_lines(path, strip=False):
 
 
 def read_file(path):
-    current_path = pathlib.Path(__file__).parent.absolute()
     data = ""
-    with open(f"{current_path}/data/{path}", "r") as file_:
+    with open(f"{CURRENT_PATH}/data/{path}", "r") as file_:
         data = file_.read()
     return data
